938457da6a35cd24eb24472fed642e90040582f2
Vsssel <asselonay@gmail.com>
2024-05-09 14:01:52 +0500

    finished singleItemPageUi

--- src/pages/SingleItemPage/ui/SingleItemPage.tsx
+++ src/pages/SingleItemPage/ui/SingleItemPage.tsx
@@ -4,11 +4,11 @@ import { SingleItemOptions } from "./SingleItemOptions";
 
 export function SingleItemPage(props: SingleItemResponse) {
   return (
-    <div className="w-100 d-flex" style={{height: '100vh'}}>
-      <div className="w-25">
+    <div className="w-100 d-flex flex-column flex-md-row" style={{height: '100vh'}}>
+      <div className="col-12 col-md-3">
         <SingleItem id={props.id} type={props.type} name={props.name} description={props.description} price={props.price}></SingleItem>
       </div>
-      <div>
+      <div className="col-12 col-md-9 mt-4">
         <SingleItemOptions></SingleItemOptions>
       </div>
     </div>
